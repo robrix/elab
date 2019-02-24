24f2a851f8ddb71ad9eb7de88978ef15c7113a6e
Rob Rix <robrix@github.com>
2019-02-24 11:58:51 -0500

    Define Elab using ReaderC & FailC.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -1,35 +1,27 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, GeneralizedNewtypeDeriving #-}
 module Elab.Check where
 
-import Control.Monad (ap)
-import Control.Monad.Fail
+import Control.Effect
+import Control.Effect.Fail
+import Control.Effect.Reader
 import qualified Data.Map as Map
 import Elab.Term (Term(..))
 import Prelude hiding (fail)
 
 type Type = Term
 
-newtype Elab v a = Elab { runElab :: [Type v] -> Map.Map v (Type v) -> Either String a }
-  deriving (Functor)
-
-instance Applicative (Elab v) where
-  pure a = Elab (const (const (Right a)))
-  (<*>) = ap
-
-instance Monad (Elab v) where
-  Elab a >>= f = Elab (\ ctx sig -> do
-    a' <- a ctx sig
-    runElab (f a') ctx sig)
-
-instance MonadFail (Elab v) where
-  fail s = Elab (const (const (Left s)))
-
+newtype Elab v a = Elab { runElab :: ReaderC [Type v] (ReaderC (Map.Map v (Type v)) (FailC VoidC)) a }
+  deriving (Applicative, Functor, Monad, MonadFail)
 
 bound :: Int -> Elab v (Type v)
-bound i = Elab (\ ctx _ -> Right (ctx !! i))
+bound i = Elab $ do
+  ctx <- ask
+  pure (ctx !! i)
 
 free :: (Ord v, Show v) => v -> Elab v (Type v)
-free v = Elab (\ _ sig -> maybe (Left ("Variable not in scope: " <> show v)) Right (Map.lookup v sig))
+free v = Elab $ do
+  sig <- ask
+  maybe (fail ("Variable not in scope: " <> show v)) pure (Map.lookup v sig)
 
 type' :: Elab v (Type v)
-type' = Elab (const (const (Right Type)))
+type' = pure Type
