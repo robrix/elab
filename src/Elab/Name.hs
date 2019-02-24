fa46759484a998e79dc3ecfedb42d7839bf6fe59
Rob Rix <robrix@github.com>
2019-02-24 12:26:53 -0500

    Add a Gensym name type.

--- src/Elab/Name.hs
+++ src/Elab/Name.hs
@@ -1,7 +1,26 @@
-{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveTraversable, FlexibleContexts #-}
 module Elab.Name where
+
+import Control.Effect
+import Control.Effect.Fresh
+import Control.Effect.Reader
 
 data Head a
   = Free a
   | Bound Int
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+
+data Gensym
+  = Root String
+  | Gensym :/ (String, Int)
+
+infixl 6 :/
+
+(//) :: Gensym -> String -> Gensym
+root // s = root :/ (s, 0)
+
+infixl 6 //
+
+gensym :: (Applicative m, Carrier sig m, Member Fresh sig, Member (Reader Gensym) sig) => String -> m Gensym
+gensym s = (:/) <$> ask <*> ((,) s <$> fresh)
