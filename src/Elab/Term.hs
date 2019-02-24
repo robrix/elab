d38cf14be9c44065946f8b0728204e4604cb4765
Rob Rix <robrix@github.com>
2019-02-23 22:33:02 -0500

    Stub in a basic term type.

--- src/Elab/Term.hs
+++ src/Elab/Term.hs
@@ -1,1 +1,11 @@
 module Elab.Term where
+
+data Term a
+  = Free a
+  | Bound Int
+  | Lam (Scope a)
+  | App (Term a) (Term a)
+  | Type
+  | Pi (Term a) (Scope a)
+
+newtype Scope a = Scope (Term a)
