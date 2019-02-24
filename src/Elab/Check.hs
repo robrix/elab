159f915e9d467def0b6afea6c1697a11ed622d0d
Rob Rix <robrix@github.com>
2019-02-24 11:10:10 -0500

    Add a signature for global vars.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -3,29 +3,30 @@ module Elab.Check where
 
 import Control.Monad (ap)
 import Control.Monad.Fail
+import qualified Data.Map as Map
 import Elab.Term
 import Prelude hiding (fail)
 
 type Type = Term
 
-newtype Elab v a = Elab { runElab :: [Type v] -> Either String a }
+newtype Elab v a = Elab { runElab :: [Type v] -> Map.Map v (Type v) -> Either String a }
   deriving (Functor)
 
 instance Applicative (Elab v) where
-  pure a = Elab (const (Right a))
+  pure a = Elab (const (const (Right a)))
   (<*>) = ap
 
 instance Monad (Elab v) where
-  Elab a >>= f = Elab (\ ctx -> do
-    a' <- a ctx
-    runElab (f a') ctx)
+  Elab a >>= f = Elab (\ ctx sig -> do
+    a' <- a ctx sig
+    runElab (f a') ctx sig)
 
 instance MonadFail (Elab v) where
-  fail s = Elab (const (Left s))
+  fail s = Elab (const (const (Left s)))
 
 
 bound :: Int -> Elab v (Type v)
-bound i = Elab (\ ctx -> Right (ctx !! i))
+bound i = Elab (\ ctx _ -> Right (ctx !! i))
 
 type' :: Elab v (Type v)
-type' = Elab (const (Right Type))
+type' = Elab (const (const (Right Type)))
