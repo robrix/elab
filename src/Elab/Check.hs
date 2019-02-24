ed318fd5e54245f5f547cd3c75161153b8769c8c
Rob Rix <robrix@github.com>
2019-02-24 09:38:19 -0500

    Define an elaborator monad.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -1,1 +1,24 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Elab.Check where
+
+import Control.Monad (ap)
+import Control.Monad.Fail
+import Elab.Term
+import Prelude hiding (fail)
+
+type Type = Term
+
+newtype Elab v a = Elab { runElab :: [Type v] -> Either String a }
+  deriving (Functor)
+
+instance Applicative (Elab v) where
+  pure a = Elab (const (Right a))
+  (<*>) = ap
+
+instance Monad (Elab v) where
+  Elab a >>= f = Elab (\ ctx -> do
+    a' <- a ctx
+    runElab (f a') ctx)
+
+instance MonadFail (Elab v) where
+  fail s = Elab (const (Left s))
