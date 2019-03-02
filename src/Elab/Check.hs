0b2dd11e32ba2c14272a05def9c767cded388d8e
Rob Rix <robrix@github.com>
2019-03-02 10:41:38 -0500

    Rename Elab to Check.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -14,23 +14,23 @@ type Type = Term
 type Context v = [Type v]
 type Signature v = Map.Map v (Type v)
 
-newtype Elab v a = Elab { runElab :: ReaderC (Context v) (ReaderC (Signature v) (ReaderC Gensym (FreshC (FailC VoidC)))) a }
+newtype Check v a = Check { runCheck :: ReaderC (Context v) (ReaderC (Signature v) (ReaderC Gensym (FreshC (FailC VoidC)))) a }
   deriving (Applicative, Functor, Monad, MonadFail)
 
-bound :: Int -> Elab v (Type v)
-bound i = Elab $ do
+bound :: Int -> Check v (Type v)
+bound i = Check $ do
   ctx <- ask
   pure (ctx !! i)
 
-free :: (Ord v, Show v) => v -> Elab v (Type v)
-free v = Elab $ do
+free :: (Ord v, Show v) => v -> Check v (Type v)
+free v = Check $ do
   sig <- ask
   maybe (fail ("Variable not in scope: " <> show v)) pure (Map.lookup v sig)
 
-lam :: Type Name -> (Name -> Elab Name (Type Name)) -> Elab Name (Type Name)
-lam ty body = Elab $ do
+lam :: Type Name -> (Name -> Check Name (Type Name)) -> Check Name (Type Name)
+lam ty body = Check $ do
   x <- Gensym <$> gensym ""
-  Term.pi (x ::: ty) <$> runElab (body x)
+  Term.pi (x ::: ty) <$> runCheck (body x)
 
-type' :: Elab v (Type v)
+type' :: Check v (Type v)
 type' = pure Type
