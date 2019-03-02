de97b46b68ec548e96b056fdf573c659b767bc0e
Rob Rix <robrix@github.com>
2019-03-02 13:31:48 -0500

    Define a Semigroup instance for Stack.

--- src/Elab/Stack.hs
+++ src/Elab/Stack.hs
@@ -5,3 +5,8 @@ data Stack a = Nil | Stack a :> a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixl 5 :>
+
+instance Semigroup (Stack a) where
+  as  <> Nil       = as
+  Nil <> bs        = bs
+  as  <> (bs :> b) = (as <> bs) :> b
