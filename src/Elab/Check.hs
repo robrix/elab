c0a8eca3c258651b9f2b579f2264cdad6af89433
Rob Rix <robrix@github.com>
2019-03-02 10:48:02 -0500

    Specialize everything to Name.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -10,30 +10,30 @@ import Elab.Term (Term(..), Typing(..))
 import qualified Elab.Term as Term
 import Prelude hiding (fail)
 
-type Type = Term
-type Context v = [Type v]
-type Signature v = Map.Map v (Type v)
+type Type = Term Name
+type Context = [Type]
+type Signature = Map.Map Name Type
 
-newtype Check v a = Check { runCheck :: ReaderC (Type v) (ReaderC (Context v) (ReaderC (Signature v) (ReaderC Gensym (FreshC (FailC VoidC))))) a }
+newtype Check a = Check { runCheck :: ReaderC Type (ReaderC Context (ReaderC Signature (ReaderC Gensym (FreshC (FailC VoidC))))) a }
   deriving (Applicative, Functor, Monad, MonadFail)
 
-newtype Infer v a = Infer { runInfer :: ReaderC (Context v) (ReaderC (Signature v) (ReaderC Gensym (FreshC (FailC VoidC)))) a }
+newtype Infer a = Infer { runInfer :: ReaderC Context (ReaderC Signature (ReaderC Gensym (FreshC (FailC VoidC)))) a }
   deriving (Applicative, Functor, Monad, MonadFail)
 
-bound :: Int -> Infer v (Type v)
+bound :: Int -> Infer Type
 bound i = Infer $ do
   ctx <- ask
   pure (ctx !! i)
 
-free :: (Ord v, Show v) => v -> Infer v (Type v)
+free :: Name -> Infer Type
 free v = Infer $ do
   sig <- ask
   maybe (fail ("Variable not in scope: " <> show v)) pure (Map.lookup v sig)
 
-lam :: Type Name -> (Name -> Check Name (Type Name)) -> Check Name (Type Name)
+lam :: Type -> (Name -> Check Type) -> Check Type
 lam ty body = Check $ do
   x <- Gensym <$> gensym ""
   Term.pi (x ::: ty) <$> runCheck (body x)
 
-type' :: Infer v (Type v)
+type' :: Infer Type
 type' = pure Type
