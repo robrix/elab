c74f7a7ebeec74d61bea8fa39a805189382dc479
Rob Rix <robrix@github.com>
2019-03-02 13:32:52 -0500

    Define a datatype to represent types.

--- src/Elab/Type.hs
+++ src/Elab/Type.hs
@@ -1,1 +1,15 @@
+{-# LANGUAGE DeriveTraversable #-}
 module Elab.Type where
+
+import Elab.Name
+import Elab.Stack
+
+data Type a
+  = Type
+  | Lam (Type a) (Scope a)
+  | Pi  (Type a) (Scope a)
+  | Head a :$ Stack (Type a)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+newtype Scope a = Scope (Type a)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
