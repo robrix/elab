a18bf02c351baaf6f7ace97518b3a99fcb3eca98
Rob Rix <robrix@github.com>
2019-03-03 14:36:56 -0500

    Define bidirectional typechecking using the elaborator DSL.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -1,1 +1,20 @@
 module Elab.Check where
+
+import Control.Monad.Fail
+import Elab.Elab
+import Elab.Name
+import Elab.Term (Term(..), instantiate)
+import Elab.Type (Typed(..))
+import Prelude hiding (fail, pi)
+
+check :: Term Name -> Check (Typed Name Value)
+check (Lam body)       = introduce (\ name -> check (instantiate (pure name) body))
+check t                = switch (infer t)
+
+infer :: Term Name -> Infer (Typed Name Value)
+infer (Head (Bound i)) = fail ("Unexpected bound variable " ++ show i)
+infer (Head (Free v))  = assumption v
+infer (f :$ a)         = infer f $$ check a
+infer Type             = type'
+infer (Pi t body)      = pi (check t) (\ name -> check (instantiate (pure name) body))
+infer t                = fail ("No rule to infer type of term " ++ show t)
