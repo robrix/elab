9cff3cb264e8d0fddb203e72a5e6a321b5c340f7
Rob Rix <robrix@github.com>
2019-03-06 14:30:58 -0500

    define a Pretty instance for [].

--- src/Elab/Pretty.hs
+++ src/Elab/Pretty.hs
@@ -1,7 +1,12 @@
 module Elab.Pretty where
 
+import Text.Show
+
 class Pretty a where
   prettys :: a -> ShowS
 
 pretty :: Pretty a => a -> String
 pretty = ($ "") . prettys
+
+instance Pretty a => Pretty [a] where
+  prettys = showListWith prettys
