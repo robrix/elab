7331ca8206766a7ec1361cf0944c83487a3e2027
Rob Rix <robrix@github.com>
2019-03-09 14:23:22 -0500

    Implement Term as a nested datatype.

--- src/Elab/Check.hs
+++ src/Elab/Check.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TypeOperators #-}
 module Elab.Check where
 
-import Control.Monad.Fail
 import Elab.Elab
 import Elab.Name
 import Elab.Term (Term(..), instantiate)
@@ -9,9 +8,8 @@ import Elab.Type (Type)
 import Prelude hiding (fail, pi)
 
 elab :: Term Name -> Elab (Value Meta ::: Type Meta)
-elab (Var (Bound i)) = fail ("Unexpected bound variable " ++ show i)
-elab (Var (Free v))  = assume v
-elab (Lam b)         = intro (\ n -> elab (instantiate (pure n) b))
-elab (f :$ a)        = elab f $$ elab a
-elab Type            = type'
-elab (Pi t b)        = pi (elab t) (\ n -> elab (instantiate (pure n) b))
+elab (Var v)  = assume v
+elab (Lam b)  = intro (\ n -> elab (instantiate (pure n) b))
+elab (f :$ a) = elab f $$ elab a
+elab Type     = type'
+elab (Pi t b) = pi (elab t) (\ n -> elab (instantiate (pure n) b))
--- src/Elab/Term.hs
+++ src/Elab/Term.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveTraversable, LambdaCase, TypeOperators #-}
+{-# LANGUAGE DeriveTraversable, LambdaCase, RankNTypes, ScopedTypeVariables, TypeOperators #-}
 module Elab.Term where
 
 import Control.Monad (ap)
@@ -6,60 +6,70 @@ import Elab.Name
 import Prelude hiding (pi)
 
 data Term a
-  = Var (Head a)
+  = Var a
   | Lam (Scope a)
   | Term a :$ Term a
   | Type
   | Pi (Term a) (Scope a)
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
-newtype Scope a = Scope (Term a)
+newtype Scope a = Scope (Term (Incr a))
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 lam :: Eq a => a -> Term a -> Term a
-lam n b = Lam (bind n b)
+lam n b = Lam (Scope (match n <$> b))
 
 lams :: (Eq a, Foldable t) => t a -> Term a -> Term a
 lams names body = foldr lam body names
 
 pi :: Eq a => a ::: Term a -> Term a -> Term a
-pi (n ::: t) b = Pi t (bind n b)
+pi (n ::: t) b = Pi t (Scope (match n <$> b))
 
 pis :: (Eq a, Foldable t) => t (a ::: Term a) -> Term a -> Term a
 pis names body = foldr pi body names
 
 
+gfoldT :: forall m n b
+       .  (forall a . m a -> n a)
+       -> (forall a . n (Incr a) -> n a)
+       -> (forall a . n a -> n a -> n a)
+       -> (forall a . n a)
+       -> (forall a . n a -> n (Incr a) -> n a)
+       -> (forall a . Incr (m a) -> m (Incr a))
+       -> Term (m b)
+       -> n b
+gfoldT var lam app ty pi dist = go
+  where go :: Term (m x) -> n x
+        go = \case
+          Var a -> var a
+          Lam (Scope b) -> lam (go (dist <$> b))
+          f :$ a -> app (go f) (go a)
+          Type -> ty
+          Pi t (Scope b) -> pi (go t) (go (dist <$> b))
+
+joinT :: Term (Term a) -> Term a
+joinT = gfoldT id (Lam . Scope) (:$) Type (\ t -> Pi t . Scope) distT
+
+distT :: Incr (Term a) -> Term (Incr a)
+distT Z     = Var Z
+distT (S t) = S <$> t
+
+
 -- | Bind occurrences of a 'Name' in a 'Term' term, producing a 'Scope' in which the 'Name' is bound.
 bind :: Eq a => a -> Term a -> Scope a
-bind name = Scope . substIn (\ i v -> Var $ case v of
-  Bound j -> Bound j
-  Free  n -> if name == n then Bound i else Free n)
+bind name = Scope . fmap (match name)
 
 -- | Substitute a 'Term' term for the free variable in a given 'Scope', producing a closed 'Term' term.
 instantiate :: Term a -> Scope a -> Term a
-instantiate image (Scope b) = substIn (\ i v -> case v of
-  Bound j -> if i == j then image else Var (Bound j)
-  Free  n -> pure n) b
-
-substIn :: (Int -> Head a -> Term b)
-        -> Term a
-        -> Term b
-substIn var = go 0
-  where go i (Var h)          = var i h
-        go i (Lam (Scope b))  = Lam (Scope (go (succ i) b))
-        go i (f :$ a)         = go i f :$ go i a
-        go _ Type             = Type
-        go i (Pi t (Scope b)) = Pi (go i t) (Scope (go (succ i) b))
+instantiate t (Scope b) = b >>= subst t . fmap Var
 
 
 instance Applicative Term where
-  pure = Var . Free
+  pure = Var
   (<*>) = ap
 
 instance Monad Term where
-  a >>= f = substIn (const (\case
-    Free a' -> f a'
-    Bound i -> Var (Bound i))) a
+  a >>= f = joinT (f <$> a)
 
 
 identityT :: Term Name
