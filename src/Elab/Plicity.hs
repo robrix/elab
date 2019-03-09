80895ff167efe0ae4eb7aacc116e9e620140c15f
Rob Rix <robrix@github.com>
2019-03-09 15:15:52 -0500

    Give Plicity a type parameter.

--- src/Elab/Plicity.hs
+++ src/Elab/Plicity.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE DeriveTraversable #-}
 module Elab.Plicity where
 
-data Plicity
-  = Im
-  | Ex
-  deriving (Eq, Ord, Show)
+data Plicity a
+  = Im a
+  | Ex a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
